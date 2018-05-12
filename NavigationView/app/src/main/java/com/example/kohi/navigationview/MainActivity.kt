47f60321e462bb95b042b54fd9f53ccdd88427ab
K-Coffee <hekouki111@gmail.com>
2018-05-12 14:07:11 +0900

    fix menu color when returned MainActivity

--- NavigationView/app/src/main/java/com/example/kohi/navigationview/MainActivity.kt
+++ NavigationView/app/src/main/java/com/example/kohi/navigationview/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.kohi.navigationview
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var checkedItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +16,12 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         setUpNavigationView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        checkedItem?.isChecked = false
     }
 
     private fun setUpNavigationView() {
@@ -27,11 +36,14 @@ class MainActivity : AppCompatActivity() {
                 R.id.menu_3 -> startActivity(Intent(this, SomethingActivity::class.java))
                 R.id.menu_4 -> startActivity(Intent(this, SomethingActivity::class.java))
             }
+            if(checkedItem != null) checkedItem?.isChecked = false
             it.isChecked = true
+            checkedItem = it
             true
         }
     }
 
+    // call fragment
     private fun trySomething() {}
 
 }
--- NavigationView/app/src/main/java/com/example/kohi/navigationview/SomethingActivity.kt
+++ NavigationView/app/src/main/java/com/example/kohi/navigationview/SomethingActivity.kt
@@ -8,5 +8,6 @@ class SomethingActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
     }
 }
